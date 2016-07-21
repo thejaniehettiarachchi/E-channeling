a7ea4deaf1390ff335351de9d44e329260d02d19
thejaniehettiarachchi <thejanie.hettiarachchi@ifsworld.com>
2016-07-21 15:36:03 +0530

    qwerty

--- Appointment.sql
+++ Appointment.sql
@@ -21,17 +21,18 @@ create procedure AddAppointment(
     vPID int,
     vDID int,
 	vSchID int,
-    vDate date,	
-	vFee double(8,2)
+    vDate date	
     )
 
 BEGIN
 
 	Declare vQueNo int;
     Declare vtime time;
+    Declare vFee double;
 
 	set vQueNo = getQueueNo(vSchID);
     set vTime = getTime(vSchID);
+    set vFee = getFee (vSchID);
     
 	insert into Appointment (
 	PID,
@@ -125,8 +126,10 @@ BEGIN
 	Declare mPatients int;
     Declare avrTime time;
     Declare appTime time;
+    Declare extraTime int; #extra time to arrive early (seconds)
     
     set QueueNo = getQueueNo(vSchID);
+    set extraTime = 600;
     
     select startTime, endTime, maxPatients into sTime, eTime, mPatients
     from Schedule
@@ -135,11 +138,37 @@ BEGIN
   # set avrTime = sec_to_time(Time_to_sec(eTime -sTime) / mPatients);
     set avrTime =(time_to_sec(eTime) -time_to_sec(sTime))/mPatients;
   #  select avrTime;
-    set appTime = sec_to_time(time_to_sec(sTime) + avrTime * (QueueNo-1) - 600);
+    set appTime = sec_to_time(time_to_sec(sTime) + avrTime * (QueueNo-1) - extraTime);
     
     return appTime;
     
 END //
+DELIMITER ;
+
+DELIMITER //
+
+create function getFee(vSchID int) returns double
+
+BEGIN
+
+	Declare Fee double(8,2);
+    Declare hosFee double(8,2); #Hospital charges
+    Declare docFee double(8,2); #Doctor's fee
+    
+    set hosFee = 100.00;
+    
+    set docFee = ( 
+		select d.Fee 
+        from Doctor as d, Schedule as s
+        where d.DID = s.DID and s.SID = vSID
+	);
+    
+    set Fee = hosFee + docFee;
+    
+    return Fee;
+
+END //
+
 DELIMITER ;
 
 
