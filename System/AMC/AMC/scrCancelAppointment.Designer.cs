f84a1d70b90e01db4aaabb21cd836199b0d646fb
Nicum Wijesuriya <nicum.wijesuriya@ifsworld.com>
2016-07-22 17:52:54 +0530

    Screens Haduwa

---
+++ System/AMC/AMC/scrCancelAppointment.Designer.cs
@@ -0,0 +1,422 @@
+﻿namespace AMC
+{
+	partial class scrCancelAppointment
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.label5 = new System.Windows.Forms.Label();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.label12 = new System.Windows.Forms.Label();
+			this.label13 = new System.Windows.Forms.Label();
+			this.label14 = new System.Windows.Forms.Label();
+			this.label15 = new System.Windows.Forms.Label();
+			this.label16 = new System.Windows.Forms.Label();
+			this.label17 = new System.Windows.Forms.Label();
+			this.label18 = new System.Windows.Forms.Label();
+			this.label19 = new System.Windows.Forms.Label();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(24, 30);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(71, 13);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Reference ID";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(101, 27);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(215, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(328, 25);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "Cancel";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(419, 25);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Update";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.label7);
+			this.groupBox1.Controls.Add(this.label9);
+			this.groupBox1.Controls.Add(this.label11);
+			this.groupBox1.Controls.Add(this.label13);
+			this.groupBox1.Controls.Add(this.label15);
+			this.groupBox1.Controls.Add(this.label17);
+			this.groupBox1.Controls.Add(this.label16);
+			this.groupBox1.Controls.Add(this.label19);
+			this.groupBox1.Controls.Add(this.label18);
+			this.groupBox1.Controls.Add(this.label14);
+			this.groupBox1.Controls.Add(this.label12);
+			this.groupBox1.Controls.Add(this.label10);
+			this.groupBox1.Controls.Add(this.label8);
+			this.groupBox1.Controls.Add(this.label6);
+			this.groupBox1.Location = new System.Drawing.Point(27, 64);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(320, 262);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Appoinment Details";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(368, 84);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(39, 13);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "Doctor";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(413, 81);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(21, 25);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(29, 13);
+			this.label3.TabIndex = 6;
+			this.label3.Text = "Start";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(21, 52);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(26, 13);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "End";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.dateTimePicker2);
+			this.groupBox2.Controls.Add(this.dateTimePicker1);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Location = new System.Drawing.Point(371, 108);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(268, 78);
+			this.groupBox2.TabIndex = 7;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Date";
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(53, 20);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker1.TabIndex = 7;
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Location = new System.Drawing.Point(53, 46);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+			this.dateTimePicker2.TabIndex = 7;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(368, 202);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(52, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Schedule";
+			this.label5.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(424, 199);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 5;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(371, 285);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(99, 25);
+			this.button3.TabIndex = 8;
+			this.button3.Text = "Save Changes";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(540, 285);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(99, 25);
+			this.button4.TabIndex = 8;
+			this.button4.Text = "Clear";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(15, 25);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(81, 13);
+			this.label6.TabIndex = 4;
+			this.label6.Text = "Patient NIC No ";
+			this.label6.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(146, 25);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(61, 13);
+			this.label7.TabIndex = 4;
+			this.label7.Text = "Patient NIC";
+			this.label7.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(15, 64);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(70, 13);
+			this.label8.TabIndex = 4;
+			this.label8.Text = "Doctor Name";
+			this.label8.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(146, 64);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(70, 13);
+			this.label9.TabIndex = 4;
+			this.label9.Text = "Doctor Name";
+			this.label9.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(15, 96);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(84, 13);
+			this.label10.TabIndex = 4;
+			this.label10.Text = "Scheduled Date";
+			this.label10.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Location = new System.Drawing.Point(146, 96);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(84, 13);
+			this.label11.TabIndex = 4;
+			this.label11.Text = "Scheduled Date";
+			this.label11.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label12
+			// 
+			this.label12.AutoSize = true;
+			this.label12.Location = new System.Drawing.Point(15, 135);
+			this.label12.Name = "label12";
+			this.label12.Size = new System.Drawing.Size(84, 13);
+			this.label12.TabIndex = 4;
+			this.label12.Text = "Scheduled Time";
+			this.label12.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label13
+			// 
+			this.label13.AutoSize = true;
+			this.label13.Location = new System.Drawing.Point(146, 135);
+			this.label13.Name = "label13";
+			this.label13.Size = new System.Drawing.Size(84, 13);
+			this.label13.TabIndex = 4;
+			this.label13.Text = "Scheduled Time";
+			this.label13.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label14
+			// 
+			this.label14.AutoSize = true;
+			this.label14.Location = new System.Drawing.Point(15, 168);
+			this.label14.Name = "label14";
+			this.label14.Size = new System.Drawing.Size(59, 13);
+			this.label14.TabIndex = 4;
+			this.label14.Text = "Queue No ";
+			this.label14.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label15
+			// 
+			this.label15.AutoSize = true;
+			this.label15.Location = new System.Drawing.Point(146, 168);
+			this.label15.Name = "label15";
+			this.label15.Size = new System.Drawing.Size(59, 13);
+			this.label15.TabIndex = 4;
+			this.label15.Text = "Queue No ";
+			this.label15.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label16
+			// 
+			this.label16.AutoSize = true;
+			this.label16.Location = new System.Drawing.Point(15, 221);
+			this.label16.Name = "label16";
+			this.label16.Size = new System.Drawing.Size(25, 13);
+			this.label16.TabIndex = 4;
+			this.label16.Text = "Fee";
+			this.label16.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label17
+			// 
+			this.label17.AutoSize = true;
+			this.label17.Location = new System.Drawing.Point(146, 221);
+			this.label17.Name = "label17";
+			this.label17.Size = new System.Drawing.Size(25, 13);
+			this.label17.TabIndex = 4;
+			this.label17.Text = "Fee";
+			this.label17.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label18
+			// 
+			this.label18.AutoSize = true;
+			this.label18.Location = new System.Drawing.Point(15, 195);
+			this.label18.Name = "label18";
+			this.label18.Size = new System.Drawing.Size(35, 13);
+			this.label18.TabIndex = 4;
+			this.label18.Text = "Room";
+			this.label18.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label19
+			// 
+			this.label19.AutoSize = true;
+			this.label19.Location = new System.Drawing.Point(146, 195);
+			this.label19.Name = "label19";
+			this.label19.Size = new System.Drawing.Size(35, 13);
+			this.label19.TabIndex = 4;
+			this.label19.Text = "Room";
+			this.label19.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// scrCancelAppointment
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(666, 340);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.groupBox2);
+			this.Name = "scrCancelAppointment";
+			this.Text = "Cancel or Update Appointment";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label11;
+		private System.Windows.Forms.Label label13;
+		private System.Windows.Forms.Label label15;
+		private System.Windows.Forms.Label label14;
+		private System.Windows.Forms.Label label12;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label17;
+		private System.Windows.Forms.Label label16;
+		private System.Windows.Forms.Label label19;
+		private System.Windows.Forms.Label label18;
+	}
+}
--- System/AMC/AMC/scrCancelAppointment.cs
+++ System/AMC/AMC/scrCancelAppointment.cs
@@ -10,19 +10,14 @@ using System.Windows.Forms;
 
 namespace AMC
 {
-	public partial class ScrSetAppointment : Form
+	public partial class scrCancelAppointment : Form
 	{
-		public ScrSetAppointment()
+		public scrCancelAppointment()
 		{
 			InitializeComponent();
 		}
 
-		private void label1_Click(object sender, EventArgs e)
-		{
-
-		}
-
-		private void radioButton3_CheckedChanged(object sender, EventArgs e)
+		private void label2_Click(object sender, EventArgs e)
 		{
 
 		}
