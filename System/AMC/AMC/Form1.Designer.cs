ce70a9ed04b60ceef6bcabef2c5abbbe9f427c82
thejaniehettiarachchi <thejanie.hettiarachchi@ifsworld.com>
2016-07-25 08:18:59 +0530

    asd

--- System/AMC/AMC/Form1.Designer.cs
+++ System/AMC/AMC/Form1.Designer.cs
@@ -29,13 +29,19 @@
 		private void InitializeComponent()
 		{
 			this.groupBox1 = new System.Windows.Forms.GroupBox();
-			this.Patient = new System.Windows.Forms.GroupBox();
 			this.button1 = new System.Windows.Forms.Button();
+			this.Patient = new System.Windows.Forms.GroupBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
 			this.groupBox1.SuspendLayout();
+			this.Patient.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// groupBox1
 			// 
+			this.groupBox1.Controls.Add(this.button3);
+			this.groupBox1.Controls.Add(this.button2);
 			this.groupBox1.Controls.Add(this.button1);
 			this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 			this.groupBox1.Location = new System.Drawing.Point(450, 75);
@@ -45,16 +51,6 @@
 			this.groupBox1.TabStop = false;
 			this.groupBox1.Text = "Doctor";
 			// 
-			// Patient
-			// 
-			this.Patient.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.Patient.Location = new System.Drawing.Point(53, 75);
-			this.Patient.Name = "Patient";
-			this.Patient.Size = new System.Drawing.Size(333, 415);
-			this.Patient.TabIndex = 1;
-			this.Patient.TabStop = false;
-			this.Patient.Text = "Patient";
-			// 
 			// button1
 			// 
 			this.button1.Location = new System.Drawing.Point(42, 73);
@@ -63,6 +59,44 @@
 			this.button1.TabIndex = 0;
 			this.button1.Text = "button1";
 			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// Patient
+			// 
+			this.Patient.Controls.Add(this.button4);
+			this.Patient.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Patient.Location = new System.Drawing.Point(53, 75);
+			this.Patient.Name = "Patient";
+			this.Patient.Size = new System.Drawing.Size(333, 415);
+			this.Patient.TabIndex = 1;
+			this.Patient.TabStop = false;
+			this.Patient.Text = "Patient";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(42, 167);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(232, 41);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(42, 265);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(232, 41);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(49, 73);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(232, 41);
+			this.button4.TabIndex = 3;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
 			// 
 			// Form1
 			// 
@@ -74,6 +108,7 @@
 			this.Name = "Form1";
 			this.Text = "Form1";
 			this.groupBox1.ResumeLayout(false);
+			this.Patient.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -83,6 +118,9 @@
 		private System.Windows.Forms.GroupBox groupBox1;
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.GroupBox Patient;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button4;
 
 	}
 }
