d98f02401cd1341a4cb1226d798157fd45fac28f
thejaniehettiarachchi <thejanie.hettiarachchi@ifsworld.com>
2016-07-25 10:07:52 +0530

    qwer

---
+++ System/AMC/AMC/AddSchedule.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace AMC
+{
+	partial class AddSchedule
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.Doctor = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.dateTimePicker4 = new System.Windows.Forms.DateTimePicker();
+			this.dateTimePicker5 = new System.Windows.Forms.DateTimePicker();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.dateTimePicker3 = new System.Windows.Forms.DateTimePicker();
+			this.groupBox3 = new System.Windows.Forms.GroupBox();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.groupBox3.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(32, 19);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(514, 131);
+			this.dataGridView1.TabIndex = 0;
+			// 
+			// Doctor
+			// 
+			this.Doctor.AutoSize = true;
+			this.Doctor.Location = new System.Drawing.Point(57, 54);
+			this.Doctor.Name = "Doctor";
+			this.Doctor.Size = new System.Drawing.Size(39, 13);
+			this.Doctor.TabIndex = 1;
+			this.Doctor.Text = "Doctor";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(113, 51);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(189, 21);
+			this.comboBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(348, 54);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(30, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Date";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// dateTimePicker1
+			// 
+			this.dateTimePicker1.Location = new System.Drawing.Point(409, 48);
+			this.dateTimePicker1.Name = "dateTimePicker1";
+			this.dateTimePicker1.Size = new System.Drawing.Size(189, 20);
+			this.dateTimePicker1.TabIndex = 4;
+			this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.dateTimePicker3);
+			this.groupBox1.Controls.Add(this.button1);
+			this.groupBox1.Controls.Add(this.dateTimePicker2);
+			this.groupBox1.Controls.Add(this.label3);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Location = new System.Drawing.Point(51, 95);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(574, 52);
+			this.groupBox1.TabIndex = 5;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Check Time";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(15, 25);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(30, 13);
+			this.label2.TabIndex = 0;
+			this.label2.Text = "From";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(244, 19);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(20, 13);
+			this.label3.TabIndex = 1;
+			this.label3.Text = "To";
+			// 
+			// dateTimePicker2
+			// 
+			this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker2.Location = new System.Drawing.Point(300, 18);
+			this.dateTimePicker2.Name = "dateTimePicker2";
+			this.dateTimePicker2.Size = new System.Drawing.Size(130, 20);
+			this.dateTimePicker2.TabIndex = 2;
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.dateTimePicker4);
+			this.groupBox2.Controls.Add(this.label6);
+			this.groupBox2.Controls.Add(this.dateTimePicker5);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Location = new System.Drawing.Point(51, 360);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(574, 52);
+			this.groupBox2.TabIndex = 6;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Schedule Details";
+			// 
+			// dateTimePicker4
+			// 
+			this.dateTimePicker4.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker4.Location = new System.Drawing.Point(62, 19);
+			this.dateTimePicker4.Name = "dateTimePicker4";
+			this.dateTimePicker4.Size = new System.Drawing.Size(130, 20);
+			this.dateTimePicker4.TabIndex = 3;
+			this.dateTimePicker4.Visible = false;
+			this.dateTimePicker4.ValueChanged += new System.EventHandler(this.dateTimePicker4_ValueChanged);
+			// 
+			// dateTimePicker5
+			// 
+			this.dateTimePicker5.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker5.Location = new System.Drawing.Point(247, 19);
+			this.dateTimePicker5.Name = "dateTimePicker5";
+			this.dateTimePicker5.Size = new System.Drawing.Size(130, 20);
+			this.dateTimePicker5.TabIndex = 2;
+			this.dateTimePicker5.Visible = false;
+			this.dateTimePicker5.ValueChanged += new System.EventHandler(this.dateTimePicker5_ValueChanged);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(217, 21);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(20, 13);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "To";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(15, 21);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(30, 13);
+			this.label5.TabIndex = 0;
+			this.label5.Text = "From";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(465, 18);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(98, 21);
+			this.button1.TabIndex = 4;
+			this.button1.Text = "Check";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(400, 21);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(92, 13);
+			this.label6.TabIndex = 7;
+			this.label6.Text = "Maximum Patients";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(498, 18);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(49, 20);
+			this.textBox1.TabIndex = 8;
+			// 
+			// dateTimePicker3
+			// 
+			this.dateTimePicker3.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+			this.dateTimePicker3.Location = new System.Drawing.Point(62, 19);
+			this.dateTimePicker3.Name = "dateTimePicker3";
+			this.dateTimePicker3.Size = new System.Drawing.Size(130, 20);
+			this.dateTimePicker3.TabIndex = 5;
+			// 
+			// groupBox3
+			// 
+			this.groupBox3.Controls.Add(this.dataGridView1);
+			this.groupBox3.Location = new System.Drawing.Point(51, 163);
+			this.groupBox3.Name = "groupBox3";
+			this.groupBox3.Size = new System.Drawing.Size(574, 180);
+			this.groupBox3.TabIndex = 7;
+			this.groupBox3.TabStop = false;
+			this.groupBox3.Text = "Available Time Slots";
+			// 
+			// AddSchedule
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(664, 532);
+			this.Controls.Add(this.groupBox3);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.dateTimePicker1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.Doctor);
+			this.Name = "AddSchedule";
+			this.Text = "Add Schedule";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.groupBox3.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.Label Doctor;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.DateTimePicker dateTimePicker4;
+		private System.Windows.Forms.DateTimePicker dateTimePicker5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.DateTimePicker dateTimePicker3;
+		private System.Windows.Forms.GroupBox groupBox3;
+	}
+}
---
+++ System/AMC/AMC/AddSchedule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AMC
+{
+	public partial class AddSchedule : Form
+	{
+		public AddSchedule()
+		{
+			InitializeComponent();
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dateTimePicker5_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void dateTimePicker4_ValueChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
